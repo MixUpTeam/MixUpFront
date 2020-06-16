5f1ba3bab66a8918553cfbedb76934b6a271d5c9
julienemo <juliette.nada@gmail.com>
2020-06-16 17:14:00 +0200

    [add] slogan and product presentation styling - moved nav css to nav specific file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
+import './styles.scss';
 import bgVideo from 'assets/videos/home-bg.mp4';
 
 const Home = () => {
   console.log('in home page');
   return (
     <>
-      <div className="page">
+      <div className='page home'>
         <video
-          playsinline="playsinline"
-          autoPlay="autoplay"
-          muted="muted"
-          loop="loop"
+          playsInline='playsinline'
+          autoPlay='autoplay'
+          muted='muted'
+          loop='loop'
+          id='homePageVideoBg'
         >
-          <source src={bgVideo} type="video/mp4" />
-          Your browser does not support the video tag.
+          <source src={bgVideo} type='video/mp4' />
+          Your browserdiv does not support the video tag.
         </video>
-        <p>A HUGE SLOGAN</p>
-        <p>present the product</p>
-        <p>huge CTA</p>
+        <div className='homePageContent'>
+          <div className='overFlowText'>
+            <p className='slogan'>Always target higher </p>
+            <p className='productPresentation'>
+              Harry étouffa à grand-peine une exclamation. Les Dursley faisaient
+              preuve d’une étonnante bêtise lorsqu’il s’agissait de leur fils
+              Dudley
+            </p>
+            <p>CTA coming soon</p>
+          </div>
+        </div>
       </div>
     </>
   );
