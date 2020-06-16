69c0a4e8f75e460f4ca504feccc3d9276ffc2f6e
julienemo <juliette.nada@gmail.com>
2020-06-16 16:30:38 +0200

    [add] background video with text over
    [change cta]

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import bgVideo from 'assets/videos/home-bg.mp4';
 
 const Home = () => {
-  return <></>;
+  console.log('in home page');
+  return (
+    <>
+      <div className="page">
+        <video
+          playsinline="playsinline"
+          autoPlay="autoplay"
+          muted="muted"
+          loop="loop"
+        >
+          <source src={bgVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <p>A HUGE SLOGAN</p>
+        <p>present the product</p>
+        <p>huge CTA</p>
+      </div>
+    </>
+  );
 };
 
 export default Home;
