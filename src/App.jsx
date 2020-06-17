c00ef1c2eb2a1632a25d6a0f07a9c88338395c7a
julienemo <juliette.nada@gmail.com>
2020-06-17 13:55:06 +0200

    [create] Playlist page and added to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Home from 'pages/Home';
 import About from 'pages/About';
 import Profile from 'pages/Profile';
 import NotFound from 'pages/NotFound';
+import Playlist from 'pages/Playlist';
 
 import Cookies from 'js-cookie';
 import { setConnexion, setProfile } from './redux';
@@ -50,7 +51,8 @@ const App = () => {
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={LogIn} />
             <Route exact path="/about" component={About} />
-
+            {/* this is a temporary link to see playlist page */}
+            <Route exact path="/temp-playlist" component={Playlist} />
             <Authroute exact path="/profile" component={Profile} />
             <Home exact path="/" component={Home} />
             <Route path="*" component={NotFound} status={404} />
---
+++ src/pages/Playlist/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Playlist = () => {
+  console.log('in page playlist');
+
+  return (
+    <>
+      <div className="page">
+        <p>This is the detail page of a playlist</p>
+      </div>
+    </>
+  );
+};
+
+export default Playlist;
