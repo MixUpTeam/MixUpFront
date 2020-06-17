1d8bea67ff4669de58c4a04cb2d04162fd4507e9
Julie K <juliette.nada@gmail.com>
2020-06-17 11:06:22 +0200

    3_home_v1 (#5)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import './styles.scss';
+import bgVideo from 'assets/videos/home-bg.mp4';
 
 const Home = () => {
-  return <></>;
+  console.log('in home page');
+  return (
+    <>
+      <div className='page home'>
+        <video
+          playsInline='playsinline'
+          autoPlay='autoplay'
+          muted='muted'
+          loop='loop'
+          id='homePageVideoBg'
+        >
+          <source src={bgVideo} type='video/mp4' />
+          Your browserdiv does not support the video tag.
+        </video>
+        <div className='homePageContent'>
+          <div className='overFlowText'>
+            <p className='slogan'>Always target higher </p>
+            <p className='productPresentation'>
+              Harry étouffa à grand-peine une exclamation. Les Dursley faisaient
+              preuve d’une étonnante bêtise lorsqu’il s’agissait de leur fils
+              Dudley
+            </p>
+            <p>CTA coming soon</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
