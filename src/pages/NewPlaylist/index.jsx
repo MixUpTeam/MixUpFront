d7d00e3015aad49fa45679cae92ea751b9951fc2
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-06-18 10:15:34 +0200

    [add] newplaylist form with material ui component + style

--- src/pages/NewPlaylist/index.jsx
+++ src/pages/NewPlaylist/index.jsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles.scss";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "50ch",
+      borderColor: "white",
+    },
+  },
+}));
 
 const NewPLaylist = () => {
+  const classes = useStyles();
+  const [input, setInput] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+  };
+
   return (
     <div className="page new-playlist-page">
-      <h1>New PLaylist</h1>
+      <h1>Create a playlist</h1>
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Choose a name"
+          variant="outlined"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="secondary"
+          id="new-playlist-button"
+        >
+          Create playlist
+        </Button>
+      </form>
     </div>
   );
 };
