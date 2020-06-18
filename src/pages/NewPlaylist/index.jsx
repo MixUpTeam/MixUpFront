e36713912bfc19d3a0028e5eca58e1ae56fc4fba
Raphaël Chappert <59604418+raphaelchpprt@users.noreply.github.com>
2020-06-18 11:43:33 +0200

    8 new playlist page (#15)

--- src/pages/NewPlaylist/index.jsx
+++ src/pages/NewPlaylist/index.jsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles.scss";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-const NewPLaylist = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "50ch",
+      borderColor: "white",
+    },
+  },
+}));
+
+const NewPlaylist = () => {
+  const history = useHistory();
+  const classes = useStyles();
+  const [input, setInput] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+    if (input) history.push("/temp-playlist");
+    // Api request : method:post, url:'/playlist'
+    // If(success) history.push("/playlist/${:playlistId}")
+  };
+
   return (
     <div className="page new-playlist-page">
-      <h1>New PLaylist</h1>
+      <h1>Create a playlist</h1>
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Choose a name"
+          variant="outlined"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="secondary"
+          id="new-playlist-button"
+        >
+          Create playlist
+        </Button>
+      </form>
     </div>
   );
 };
 
-export default NewPLaylist;
+export default NewPlaylist;
