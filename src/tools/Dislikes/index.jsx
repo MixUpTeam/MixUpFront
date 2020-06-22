d29135d7f2917ae451e2ed27c44414087d8488d9
floriansr <sueurfloriansf@gmail.com>
2020-06-22 16:49:18 +0200

    [fixup] Dislikes tools, refacto

--- src/tools/Dislikes/index.jsx
+++ src/tools/Dislikes/index.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import APIManager from 'services/APIManager';
 
-const Dislikes = () => {
-  return (
-    <>
-    </>
-  );
+const Dislikes = async (track_id, user_id) => {
+  const res = await APIManager.downVote(track_id, user_id);
+  console.log(res);
+  return res;
 };
 
 export default Dislikes;
--- src/tools/Likes/index.jsx
+++ src/tools/Likes/index.jsx
@@ -2,7 +2,6 @@ import APIManager from 'services/APIManager';
 
 const Likes = async (track_id, user_id) => {
   const res = await APIManager.upVote(track_id, user_id);
-  console.log(res);
   return res;
 };
 
