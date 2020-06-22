3bca1855fb90a0caaea59048ee80357fd4a8b149
floriansr <sueurfloriansf@gmail.com>
2020-06-22 15:27:34 +0200

    [add] api_manager upVote method added

--- src/pages/NewPlaylist/index.jsx
+++ src/pages/NewPlaylist/index.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
-import "./styles.scss";
-import { useHistory } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import APIManager from "services/APIManager";
-import { message } from "antd";
+import React, { useState } from 'react';
+import './styles.scss';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import APIManager from 'services/APIManager';
+import { message } from 'antd';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    "& > *": {
+    '& > *': {
       margin: theme.spacing(1),
-      width: "50ch",
-      borderColor: "white",
+      width: '50ch',
+      borderColor: 'white',
     },
   },
 }));
@@ -27,9 +27,10 @@ const NewPlaylist = () => {
     if (input) {
       const res = await APIManager.createPlaylist(1, input);
       console.log(res);
-      if (res.status === "success") return history.push(`/playlist/${res.id}`);
-      else return message.error("Oops, please try again!", 3);
-    } else return message.error("Name can't be blank...", 3);
+      if (res.status === 'success') return history.push(`/playlist/${res.id}`);
+      return message.error('Oops, please try again!', 3);
+    }
+    return message.error("Name can't be blank...", 3);
   };
 
   return (
--- src/pages/Playlist/index.jsx
+++ src/pages/Playlist/index.jsx
@@ -1,30 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-import "./styles.scss";
-import APIManager from "services/APIManager";
-import { makeStyles } from "@material-ui/core/styles";
+import './styles.scss';
+import APIManager from 'services/APIManager';
+import { makeStyles } from '@material-ui/core/styles';
 
-import TextField from "@material-ui/core/TextField";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import Button from "@material-ui/core/Button";
-import { message, Card } from "antd";
-import {
-  LikeOutlined
-} from '@ant-design/icons';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Button from '@material-ui/core/Button';
+import { message, Card } from 'antd';
+import { LikeOutlined } from '@ant-design/icons';
 
-import Likes from 'tools/Likes'
-import Dislikes from 'tools/Dislikes'
+import Likes from 'tools/Likes';
+import Dislikes from 'tools/Dislikes';
 
-
-import ShortID from "shortid";
+import ShortID from 'shortid';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    "& > *": {
+    '& > *': {
       margin: theme.spacing(1),
-      width: "50ch",
-      borderColor: "white",
+      width: '50ch',
+      borderColor: 'white',
     },
   },
 }));
@@ -35,29 +32,30 @@ const Playlist = () => {
   const { playlistId } = useParams();
 
   const [userTrackChoice, setUserTrackChoice] = useState(null);
-  const [playlist, setPlaylist] = useState("");
-  const [tracks, setTracks] = useState("");
+  const [playlist, setPlaylist] = useState('');
+  const [tracks, setTracks] = useState('');
 
   const tempFakeSuggestions = [
     {
-      id: "this is the first choice",
+      id: 'this is the first choice',
     },
     {
-      id: "second choice is this",
+      id: 'second choice is this',
     },
     {
-      id: "yet here is a third one",
+      id: 'yet here is a third one',
     },
     {
-      id: "about time to have a fourth",
+      id: 'about time to have a fourth',
     },
     {
-      id: "be careful, fifth coming",
+      id: 'be careful, fifth coming',
     },
   ];
 
   useEffect(() => {
     const fetchPlaylist = async () => {
+      console.log('fetchPlaylist -> playlistId', playlistId);
       const res = await APIManager.showPlaylist(playlistId);
       setPlaylist(res);
       setTracks(res.entries);
@@ -67,13 +65,17 @@ const Playlist = () => {
 
   const searchBarOnSubmit = async (e) => {
     e.preventDefault();
-    if (!userTrackChoice) return message.error("Please choose a track");
+    console.log('searchBarOnSubmit -> userId', userId);
+    console.log('searchBarOnSubmit -> userTrackChoice', userTrackChoice);
+    console.log('searchBarOnSubmit -> playlistId', playlistId);
+
+    if (!userTrackChoice) return message.error('Please choose a track');
     const res = await APIManager.addTrackToPlaylist(
       userId,
       userTrackChoice,
-      playlistId,
+      playlistId
     );
-    if (res.status === "error") return message.error(res.messages[0]);
+    if (res.status === 'error') return message.error(res.messages[0]);
     setTracks([...tracks, res]);
   };
 
@@ -96,7 +98,7 @@ const Playlist = () => {
               id="suggestion-list"
               options={tempFakeSuggestions}
               getOptionLabel={(option) => option.id}
-              style={{ width: "100%" }}
+              style={{ width: '100%' }}
               onChange={inputOnChange}
               renderInput={(params) => (
                 <TextField
@@ -123,8 +125,8 @@ const Playlist = () => {
           tracks.map((track) => (
             <Card key={ShortID.generate()} style={{ width: 300 }}>
               <p>{track.track_spotify_id}</p>
-              <LikeOutlined onClick={() => Likes(track.id, 11)}/> 
-              <LikeOutlined rotate={180} onClick={Dislikes}/>
+              <LikeOutlined onClick={() => Likes(track.id, 11)} />
+              <LikeOutlined rotate={180} onClick={Dislikes} />
             </Card>
           ))}
       </div>
