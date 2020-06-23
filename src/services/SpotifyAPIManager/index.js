de5b95b8970d5026f42b8ffb76f10eca242858ef
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-06-23 10:48:47 +0200

    refresh token

--- src/services/SpotifyAPIManager/index.js
+++ src/services/SpotifyAPIManager/index.js
@@ -11,7 +11,11 @@ API.interceptors.request.use(
       ...headers,
       "Content-Type": "application/json",
       Authorization:
+<<<<<<< HEAD
         "Bearer BQCo1sYa96Nx1mpgwmAqr5CqkzA77MHWibqhQVLzQsAGS2UwIS4tqNN4rwYLDleSep4pSXXirPCzQLO1NoN-_xDItAVoMrKyqU2Vk79zIvezx8RdEW04lqObILU6vgitvBRyxCoMdpl_fqO0wvKhm7pS5enCQGMAVfAq5c01RLU6dXJ9KRYKMw2csp7gEQgonGDZid12EIbdlJ_E2J25geWkOfaWE2JjBLaS_W50-irYOtAuYHI4EMg5QLeWx430FoW7lTlkrvvgI_g",
+=======
+        "Bearer BQDnFGiKJA3JrVbmGjmM1MrYnovxP5_pKrwoLqMPAQd8Jrs0Lu7OFGFJPBDLQGwNCXLSvpnullsu7wFRouzGm1LTlNqHPSe_3FGudANPbPUSZdj95RuvzxN_jEPbPks8FxC1eDyWvDftMlaXZm2UEJDnPGKLrOa5ND6TT3H85HAgwpTrAtlfMjW292KYRaFVAFrUZl4yZjlmrWFb7cYP211LWzq3b8mCRFFC_o3oK4Y4v7kvaqTio_ZMKvu2vKoME_tSbbM6s-oVCR8",
+>>>>>>> e0f8217... refresh token
     },
   }),
   (error) => {
