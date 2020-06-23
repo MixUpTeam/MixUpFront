02f41772c7d6b1c7c8ce51393f74ae6d4e53223d
julienemo <juliette.nada@gmail.com>
2020-06-23 19:29:05 +0200

    [delete] redundant sign_in request

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,20 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import Navbar from "components/Navbar";
-import Authroute from "tools/Authroute";
+import Navbar from 'components/Navbar';
+import Authroute from 'tools/Authroute';
 
-import Register from "pages/Register";
-import LogIn from "pages/Login";
-import Home from "pages/Home";
-import About from "pages/About";
-import Profile from "pages/Profile";
-import NotFound from "pages/NotFound";
-import Playlist from "pages/Playlist";
-import NewPlaylist from "pages/NewPlaylist";
-
-import Cookies from "js-cookie";
-import { setConnection, setProfile } from "./redux";
+import Register from 'pages/Register';
+import LogIn from 'pages/Login';
+import Home from 'pages/Home';
+import About from 'pages/About';
+import Profile from 'pages/Profile';
+import NotFound from 'pages/NotFound';
+import Playlist from 'pages/Playlist';
+import NewPlaylist from 'pages/NewPlaylist';
 
 const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (Cookies.get("token")) {
-      const token = JSON.parse(Cookies.get("token")).jwt;
-
-      fetch("https://form-you-back.herokuapp.com/users/sign_in.json", {
-        method: "post",
-        headers: {
-          Authorization: token,
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          if (!response.error) {
-            dispatch(setProfile(response));
-            dispatch(setConnection());
-          }
-        })
-        .catch((error) => console.error(error));
-    }
-  }, [dispatch]);
-
   return (
     <>
       <Router>
