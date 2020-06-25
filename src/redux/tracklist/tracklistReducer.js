02918e7b2479cc26050d3a8134e2eb06dad96234
Yousri <laifayousri@gmail.com>
2020-06-25 15:38:14 +0200

    [add] redux state: currentTrack

--- src/redux/tracklist/tracklistReducer.js
+++ src/redux/tracklist/tracklistReducer.js
@@ -1,9 +1,14 @@
-import { SET_TRACKS, CREATE_TRACK } from 'redux/tracklist/tracklistType';
+import {
+  SET_TRACKS,
+  CREATE_TRACK,
+  SET_CURRENT_TRACK
+} from 'redux/tracklist/tracklistType';
 
 const initialState = {
   tracks: [],
   name: null,
   owner: null,
+  currentTrack: null,
 };
 
 const tracklistReducer = (state = initialState, action) => {
@@ -12,13 +17,18 @@ const tracklistReducer = (state = initialState, action) => {
       return {
         ...state,
         tracks: action.details,
-        name: action.name,
-        owner: action.owner,
+          name: action.name,
+          owner: action.owner,
       };
     case CREATE_TRACK:
       return {
         ...state,
         tracks: state.tracks.concat(action.details),
+      };
+    case SET_CURRENT_TRACK:
+      return {
+        ...state,
+        currentTrack: action.currentTrack,
       };
     default:
       return state;
--- src/redux/tracklist/tracklistType.js
+++ src/redux/tracklist/tracklistType.js
@@ -1,2 +1,3 @@
 export const SET_TRACKS = 'SET_TRACKS';
 export const CREATE_TRACK = 'CREATE_TRACK';
+export const SET_CURRENT_TRACK = 'SET_CURRENT_TRACK';
