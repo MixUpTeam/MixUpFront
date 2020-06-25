be2ee4846dec543dbe7d9f6daba58a5e4c9a16de
floriansr <sueurfloriansf@gmail.com>
2020-06-25 15:54:07 +0200

    [add] display songs with same order than tracklist

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import SpotifyPlayer from 'react-spotify-player';
+import { useSelector } from 'react-redux';
+import SpotifyPlayer from 'react-spotify-web-playback';
 
-const Player = ( {spotifyDetails} ) => {
-  console.log(spotifyDetails);
-  const size = {
-    width: '100%',
-    height: 300,
-  };
-  const view = 'list'; // or 'coverart'
-  const theme = 'black'; // or 'white'
+const Player = () => {
+  const tracklist = useSelector((state) => state.tracks.tracks);
+
+  const token =
+    'BQDmCYAa-R1A4Gjli3_ZLFP7u_RB9dR2-LuZX8qa1eYoypwRr0_VOZlN09n9qQOy20Nk1Pwv9qslblzNCXOqnVJCTQT44Uz-7s_5Fnk2idNUieGMayTuIgbpFRBs4yVxzjM4Mw4rwQTpDUhCKFJjUTNaKi4gvbkqfClYnISBg8wZ28Wnw28IHD8lmQKqNN7CSHijJNSjCXJLlvQ';
+
+  const uris = [];
+  tracklist
+    .sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at))
+    .sort((a, b) => b.score - a.score)
+    .map((x) => {
+      uris.push(`spotify:track:${x.track_spotify_id}`);
+    });
 
   return (
     <>
-      <SpotifyPlayer
-        uri={spotifyDetails[0].uri}
-        size={size}
-        view={view}
-        theme={theme}
-      />
+      <SpotifyPlayer autoPlay token={token} uris={uris} />
     </>
   );
 };
