3801fd509f190ea9ca86f19a3d748d6485d06ade
Julie K <juliette.nada@gmail.com>
2020-06-26 19:37:32 +0200

    PWA (#72)
    * [add] service worker
    
    * [add] different formats of logo
    
    * [add] manifest.json
    
    * [add] pwa related tags to html
    
    * [fix] manifest and html default color settings

---
+++ public/worker.js
@@ -0,0 +1,18 @@
+const cacheName = 'mixup-pwa';
+const filesToCache = ['/', '/index.html', '/css/style.css', '/js/main.js'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
