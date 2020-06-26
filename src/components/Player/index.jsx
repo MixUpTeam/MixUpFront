f5f3c920387a8d2647bb2b2d4a2fc7190ddd5606
julienemo <juliette.nada@gmail.com>
2020-06-26 19:30:42 +0200

    [BREAKTHROUGH] identified the end of current track

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,39 +1,25 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
 import SpotifyPlayer from 'react-spotify-web-playback';
 
 import './styles.scss';
-import { setCurrentTrack } from '../../redux';
 
 const Player = ({ spotifyTrack, trackPlaylistId }: Player) => {
-  console.log('Player -> trackPlaylistId', trackPlaylistId);
-  const dispatch = useDispatch();
-  const tracklist = useSelector((state) => state.tracks.tracks);
   const token =
-    'BQDabsv0fjBu7tmTx0kaURtOMZMUTju36N1z5Io_12h12WAFS2n9ca-sALOGAfattKaMknnwKKrjimpwEqEM_LgLWVraQBx_neKXAqmQgRErnEFqj0gr6IcX_waith_blgUISkBY1Cz5xW92PoaWc5EPwZXy-xf8rnXjVPrvbEqqv6jXB9dGz8LttwgORNnVa_IjELclYdCpPTU';
-
-  const test = () => {
-    const nextSong = tracklist[0];
-    console.log('test -> nextSong', nextSong);
-
-    dispatch(setCurrentTrack(nextSong));
-  };
-
-  useEffect(() => {
-    if (spotifyTrack) {
-      setInterval(test, spotifyTrack.duration_ms - 38000);
-    }
-  }, [spotifyTrack]);
-
-  console.log([`${spotifyTrack.uri}`, 'spotify:track:55p8TQ1ggGYOO1gLQrC52D']);
+    'BQBD9lldPwvQUK7q-88v2V5YScS4aAjhDdEaYgqvcWuNyfw7Ve__TNFAHF4taE45Gm44-cc96LC2uEIlfbH4gAWWm-fonOKh_tzJN2RuTA_J4zvrUrGvumm3Ucg8rrLuBBhHw0IQ1jG6R1R4ReLHmuvgZCWqoU3KfDTo1kMon-j47AQuX-YiQ8DNpgrHRB0t74OO0kUBrHuKDDo';
 
   const renderedItem = () => {
     return (
       <>
         <SpotifyPlayer
           autoPlay
+          offset={1}
           token={token}
-          uris={[`${spotifyTrack.uri}`, 'spotify:track:55p8TQ1ggGYOO1gLQrC52D']}
+          uris={['spotify:track:55p8TQ1ggGYOO1gLQrC52D', `${spotifyTrack.uri}`]}
+          callback={(e) => {
+            if (e.position >= 99.5) {
+              console.log(`resque fini ${e.position}`);
+            }
+          }}
         />
       </>
     );
